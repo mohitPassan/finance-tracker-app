73d187980ca6bb30ad0b6347e3a7758045e85587
Mohit Passan <mohitpassan@gmail.com>
2024-10-25 00:36:57 +0530

    Implemented authentication using next auth. Using Google sign in for now

---
+++ auth.ts
@@ -0,0 +1,16 @@
+import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
+import PostgresAdapter from "@auth/pg-adapter";
+import { Pool } from "pg";
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    max: 20,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 2000,
+});
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+    adapter: PostgresAdapter(pool),
+    providers: [Google],
+});
--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,14 @@ const nextConfig = {
             {
                 source: "/api/:path*",
                 destination: `${process.env.SERVER_URL}/api/:path*`,
+                // Exclude paths that start with /api/auth
+                has: [
+                    {
+                        type: "host",
+                        key: "pathname",
+                        value: "/api/(?!auth)",
+                    },
+                ],
             },
         ];
     },
